ce34371afc3daf1070af1dad3106c4fedf82daab
nguyen dinh chien <Ndc7571@gmail.com>
2025-04-14 20:10:36 +0700

    optimiz call api

--- src/app/component/user-manager/user-manager.component.ts
+++ src/app/component/user-manager/user-manager.component.ts
@@ -1,37 +1,15 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
 import { MatDialog } from "@angular/material/dialog";
 import { UserFormModalComponent } from "../user-form-modal/user-form-modal.component";
-import { CommonModule } from "@angular/common";
-import { MatIconModule } from "@angular/material/icon";
-import { MatTableModule } from "@angular/material/table";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatButtonModule } from "@angular/material/button";
-import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatSnackBar, MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatTableDataSource } from "@angular/material/table";
 import { MatPaginator } from "@angular/material/paginator";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { UserService } from "../../services/user.service";
 
 @Component({
   selector: "app-user-manager",
-  standalone: true,
   templateUrl: "./user-manager.component.html",
   styleUrls: ["./user-manager.component.css"],
-  imports: [
-    CommonModule,
-    HttpClientModule,
-    MatIconModule,
-    MatSnackBarModule,
-    MatTableModule,
-    MatFormFieldModule,
-    MatPaginatorModule,
-    MatInputModule,
-    MatButtonModule,
-    FormsModule,
-  ],
 })
 export class UserManagerComponent implements OnInit {
   users: any[] = [];
@@ -41,7 +19,7 @@ export class UserManagerComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(
-    private http: HttpClient,
+    private userService: UserService, // Inject UserService
     private dialog: MatDialog,
     private snackBar: MatSnackBar
   ) {}
@@ -63,7 +41,7 @@ export class UserManagerComponent implements OnInit {
   }
 
   loadUsers() {
-    this.http.get<any>("https://reqres.in/api/users").subscribe((res) => {
+    this.userService.getUsers().subscribe((res) => {
       this.users = res.data.map((user: any) => ({
         id: user.id,
         name: `${user.first_name} ${user.last_name}`,
@@ -94,38 +72,34 @@ export class UserManagerComponent implements OnInit {
   addUser(newUser: any) {
     if (!newUser.name || !newUser.email) return;
 
-    this.http
-      .post<any>("https://reqres.in/api/users", newUser)
-      .subscribe(() => {
-        this.showNotification("Added successfully");
+    this.userService.addUser(newUser).subscribe(() => {
+      this.showNotification("Added successfully");
 
-        const fakeId = Math.floor(Math.random() * 10000);
-        const addedUser = { id: fakeId, ...newUser };
+      const fakeId = Math.floor(Math.random() * 10000);
+      const addedUser = { id: fakeId, ...newUser };
 
-        this.dataSource.data = [...this.dataSource.data, addedUser];
-      });
+      this.dataSource.data = [...this.dataSource.data, addedUser];
+    });
   }
 
   updateUser(updatedUser: any) {
     if (!updatedUser) return;
 
-    this.http
-      .put(`https://reqres.in/api/users/${updatedUser.id}`, updatedUser)
-      .subscribe(() => {
-        this.showNotification("Updated successfully");
+    this.userService.updateUser(updatedUser).subscribe(() => {
+      this.showNotification("Updated successfully");
 
-        const index = this.dataSource.data.findIndex(
-          (user: any) => user.id === updatedUser.id
-        );
-        if (index !== -1) {
-          this.dataSource.data[index] = { ...updatedUser };
-          this.dataSource.data = [...this.dataSource.data];
-        }
-      });
+      const index = this.dataSource.data.findIndex(
+        (user: any) => user.id === updatedUser.id
+      );
+      if (index !== -1) {
+        this.dataSource.data[index] = { ...updatedUser };
+        this.dataSource.data = [...this.dataSource.data];
+      }
+    });
   }
 
   deleteUser(id: number) {
-    this.http.delete(`https://reqres.in/api/users/${id}`).subscribe(() => {
+    this.userService.deleteUser(id).subscribe(() => {
       this.showNotification("Deleted successfully");
 
       this.dataSource.data = this.dataSource.data.filter(
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class UserService {
+  private apiUrl = "https://reqres.in/api/users";
+
+  constructor(private http: HttpClient) {}
+
+  // Lấy danh sách người dùng
+  getUsers(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  // Thêm người dùng mới
+  addUser(newUser: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, newUser);
+  }
+
+  // Cập nhật thông tin người dùng
+  updateUser(updatedUser: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${updatedUser.id}`, updatedUser);
+  }
+
+  // Xóa người dùng
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
+}
